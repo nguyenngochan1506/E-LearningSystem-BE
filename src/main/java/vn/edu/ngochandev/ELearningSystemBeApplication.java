f6043302b92f3be124cf253dbde7d5be611d2eb0
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2025-02-21 16:50:22 +0700

    init project

---
+++ src/main/java/vn/edu/ngochandev/ELearningSystemBeApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.ngochandev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningSystemBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/ngochandev/ELearningSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ngochandev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
