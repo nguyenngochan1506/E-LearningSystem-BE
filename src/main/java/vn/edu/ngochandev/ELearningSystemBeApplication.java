aa8928bbf142487541aae08656c20f0cff57c447
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2025-02-21 18:23:39 +0700

    xây dựng project base, config api document với swagger

--- src/main/java/vn/edu/ngochandev/ELearningSystemBeApplication.java
+++ src/main/java/vn/edu/ngochandev/ELearningSystemBeApplication.java
@@ -1,13 +1,14 @@
 package vn.edu.ngochandev;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ELearningSystemBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ELearningSystemBeApplication.class, args);
 	}
+
 
 }
